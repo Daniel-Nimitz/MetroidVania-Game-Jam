b1dfb1183b2acefc4bfe5ea81054d95b46999587
Daniel-Nimitz <63825442+Daniel-Nimitz@users.noreply.github.com>
2020-05-14 15:24:42 -0600

    Second Tree and movement
    Added Second tree
    In main scene we have a player
    player is blue
    blue material created
    playermovement script started

---
+++ MetroidVania/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float horizontalInput;
+    [SerializeField] float verticalInput;
+    public Rigidbody playerBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput =Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        
+        Vector3 inputVector = new Vector3(horizontalInput * speed, playerBody.velocity.y, verticalInput * speed);
+       
+        //Will need a way to jump
+    }
+}
