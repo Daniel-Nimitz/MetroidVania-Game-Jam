990d83373834c7014df559c82c8f2f7e51483fd3
Trumpet014 <64665777+Trumpet014@users.noreply.github.com>
2020-05-16 16:57:58 -0700

    Notes to self on the script

--- MetroidVania/Assets/Scripts/PlayerGlide.cs
+++ MetroidVania/Assets/Scripts/PlayerGlide.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerGlide : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    //Define some private variables here for changes on gravity and directional force.
     void Start()
     {
         
@@ -13,6 +15,16 @@ public class PlayerGlide : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    //This will change the player's gravity
+    /* Note to Alex: change the players gravity as an if or while loop(if player holds down space).
+     * Player gravity = rigidbody (weight) of player by probably 75% until they land on the ground. 
+     * Maybe even make a little updraft = Force in upwards direction.
+     * Cancel when player hits the ground or a dropbox. */
+    
+    //This will allow the player to change direction while gliding
+    /*Change the way the arrow keys works but just for the glide.
+     * Potentially have them add force to the players direction.
+     * Gliding will be kind of like controlling the win. */
+
     }
 }
