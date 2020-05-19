bd654c2e9640d1f4c3ccf56b08710702c65969c6
Daniel-Nimitz <63825442+Daniel-Nimitz@users.noreply.github.com>
2020-05-19 16:44:02 -0600

    Committing tanner's changes try #2

---
+++ MetroidVania/Assets/Scripts/camSwitch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camSwitch : MonoBehaviour
+{
+    //this calls the animator so we can refrence it below
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //I honestly don't know what this does but its important
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("o"))
+        {
+            //moves the camera up to the sky
+            anim.SetBool("camTransition", true);
+        }
+        if (Input.GetKeyDown("t"))
+        {
+            //moves the camera back down to the player
+            anim.SetBool("camTransition", false);
+        }
+    }
+
+}
