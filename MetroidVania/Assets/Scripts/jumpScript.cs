5be81538e1e5b804773e1b7fa518189a8bf431ca
Daniel-Nimitz <63825442+Daniel-Nimitz@users.noreply.github.com>
2020-05-22 17:34:04 -0600

    Jump scripts manually added

---
+++ MetroidVania/Assets/Scripts/jumpScript.cs
@@ -0,0 +1,40 @@
+﻿
+using UnityEngine;
+
+public class jumpScript : MonoBehaviour
+{
+    public float fallMultiple = 2.5f;
+    Rigidbody playerPhys;
+    public float jumpForce = 520f;
+    private bool grounded;
+    public BoxCollider col;
+    public LayerMask groundLayers;
+    public float heightOffset = 1.0f;
+    public float groundedHeight = 1.0f;
+
+
+    void Start()
+    {
+        playerPhys = GetComponent<Rigidbody>();
+        col = GetComponent<BoxCollider>();
+        InvokeRepeating("GroundCheck", 0, 0.1f);
+    }
+
+    // The jump action
+    void Update()
+    {
+        if (grounded == true && Input.GetKeyDown(KeyCode.Space))
+            playerPhys.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    //Function to check if the player is grounded.
+    void GroundCheck()
+    {
+        if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y + heightOffset, transform.position.z), Vector3.down, groundedHeight + heightOffset, groundLayers))
+            grounded = true;
+        else
+            grounded = false;
+
+
+    }
+}
