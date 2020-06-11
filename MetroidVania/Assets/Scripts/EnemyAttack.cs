3d8d0f0f486f96428e8c9b68c9041c0bd514894d
Daniel-Nimitz <63825442+Daniel-Nimitz@users.noreply.github.com>
2020-06-11 12:47:22 -0600

    enemy attack script started

---
+++ MetroidVania/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float attackRange;
+    public GameObject target;
+    public GameObject recipeUI;
+
+    private recipeManager recipeScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        recipeScript = recipeUI.GetComponent<recipeManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float playerDistance = Vector3.Distance(transform.position, target.transform.position);
+        if (playerDistance <= attackRange) {
+            Attack();
+        }
+    }
+    public void Attack() {
+        print("Player has been attacked");
+    
+    }
+}
