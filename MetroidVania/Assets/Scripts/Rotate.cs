a3a5feb8b74fbed6e283a90edb0f7fce94d0cc86
Daniel-Nimitz <63825442+Daniel-Nimitz@users.noreply.github.com>
2020-06-11 00:52:52 -0600

    Added Rotating script and attached to log

---
+++ MetroidVania/Assets/Scripts/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Rotate : MonoBehaviour
+{
+    public float rotationSpeed = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotationSpeed);
+    }
+}
